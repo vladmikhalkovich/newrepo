c107ed5d1cc352efb89971532248f14fc9d09fc7
vlad <vlad.mikhalkovich@gmail.com>
2019-12-18 21:41:25 +0300

    enable cors

--- src/main/java/by/mikhalkovich/training_center_v2/security/SecurityConfig.java
+++ src/main/java/by/mikhalkovich/training_center_v2/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable();
+        //http.cors().disable();
         http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
